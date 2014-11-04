92ffd48aaccf996fa0579400f799aa1273940943
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2014-11-04 13:13:29 +0530

    Working script, downloads blog rss, checks with old copy, dialog notif if different. TODO: Parse rss and display new post headings as dialog msg

---
+++ blog-notify.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+file="auth.txt"
+
+rsslink="http://placements.iitb.ac.in/blog/?feed=rss2"
+bloglink="http://placements.iitb.ac.in/blog/"
+
+while read -r a b; do
+    myname=$a
+    mypasswd=$b    
+done < $file
+
+wget --quiet --user "$myname" --password "$mypasswd" "$rsslink" -O new.html
+#cp oldref.html old.html
+#echo "new" > new.html
+
+differ=false
+#Logic here
+cmp --silent new.html old.html || differ=true
+
+echo $differ
+if [ $differ == true ]
+then
+    zenity --question --text="There is something new on the blog" --ok-label="Finally! Open the Blog" --cancel-label="Meh"
+if [ $? == 0 ] ; then
+    google-chrome $bloglink
+fi
+fi
+
+mv new.html old.html
